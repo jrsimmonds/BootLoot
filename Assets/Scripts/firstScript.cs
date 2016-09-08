f8ca26e5da92533412901725f56108c5fa82c3b4
Joe Simmonds <joe.simmonds@sit.ac.nz>
2016-09-08 13:55:35 +1200

    small changes
    small changes

---
+++ Assets/Scripts/firstScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class firstScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
