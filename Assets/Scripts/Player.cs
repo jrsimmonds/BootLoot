5e4586a6c0fda5319ba13a46dbb6bb810a88206f
Joe Simmonds <joe.simmonds@sit.ac.nz>
2016-09-27 16:06:10 +1300

    map
    main map added with colliders and Player added to test colliders and
    map layout.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,243 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public Rigidbody2D MyRigidbody { get; set; }
+
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private bool moving = false;
+
+    public Animator MyAnimator;
+
+    public bool facingRight = false;
+
+    [SerializeField]
+    private Transform[] groundPoints;
+
+    [SerializeField]
+    private float groundRadius;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    [SerializeField]
+    private bool isGrounded;
+
+    private bool jump;
+
+    [SerializeField]
+    private bool kick;
+    private bool kicking;
+
+    [SerializeField]
+    private float jumpForce;
+
+    [SerializeField]
+    private bool AirControl;
+
+	[SerializeField]
+	public bool hurtControlsOff;
+
+    // Use this for initialization
+    void Start () {
+        MyRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        HandleInput();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float horizontal = Input.GetAxis("Horizontal");
+        if(horizontal != 0)
+        {
+            moving = true;
+        }else
+        {
+            moving = false;
+        }
+        isGrounded = IsGrounded();
+        HandleMovement(horizontal);
+        Flip(horizontal);
+        HandleLayers();
+        Reset();
+    }
+
+    void HandleMovement(float horizontal)
+    {
+		if (!hurtControlsOff) {
+
+			if (MyRigidbody.velocity.y < 0) {
+				MyAnimator.SetBool ("Land", true);
+			}
+
+			if (isGrounded) {
+				MyAnimator.SetBool ("Land", false);
+			}
+
+			if (!kicking && (isGrounded || AirControl)) {
+				MyRigidbody.velocity = new Vector2 (horizontal * movementSpeed, MyRigidbody.velocity.y);
+			}
+
+			//JUMP CODE
+			if (moving && isGrounded && jump) {
+				if (isGrounded && jump) {
+					isGrounded = false;
+					if (facingRight) {
+						MyRigidbody.AddForce (new Vector2 (100, jumpForce));
+						MyAnimator.SetTrigger ("Jump");
+					} else if (!facingRight) {
+						MyRigidbody.AddForce (new Vector2 (-100, jumpForce));
+						MyAnimator.SetTrigger ("Jump");
+					}
+				}
+			} else if (!moving && isGrounded && jump) {
+				MyRigidbody.AddForce (new Vector2 (0, jumpForce));
+				MyAnimator.SetTrigger ("Jump");
+			}
+			/////JUMP CODE FINISHED
+
+			//KICK CODE
+			if (kick) {
+				MyAnimator.SetBool ("Kick", true);
+			}
+			/////KICK CODE FINISHED
+
+			MyAnimator.SetFloat ("Speed", Mathf.Abs (horizontal));
+		}
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            kick = true;
+            kicking = true;
+            //MyRigidbody.velocity = Vector2.zero;
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            MyAnimator.SetTrigger("Hurt");
+            GameObject.Find("Globals").GetComponent<globals>().health = GameObject.Find("Globals").GetComponent<globals>().health - 1;
+        }
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            ChangeDirection();
+        }
+    }
+
+    public virtual void ChangeDirection()
+    {
+        //Changes the facingRight bool to its negative value
+        facingRight = !facingRight;
+
+        //Flips the character by changing the scale
+        transform.localScale = new Vector3(transform.localScale.x * -1, 1, 1);
+    }
+
+    private bool IsGrounded()
+    {
+        if(MyRigidbody.velocity.y <= 0)
+        {
+            foreach (Transform point in groundPoints)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    if(colliders[i].gameObject != gameObject)
+                    {
+                        MyAnimator.ResetTrigger("Jump");
+                        MyAnimator.SetBool("Land", false);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private void HandleLayers()
+    {
+        if (!isGrounded)
+        {
+            MyAnimator.SetLayerWeight(1, 1);
+        }
+        else
+        {
+            MyAnimator.SetLayerWeight(1, 0);
+        }
+    }
+
+    void Reset()
+    {
+        jump = false;
+        kick = false;
+    }
+
+    private void kicker()
+    {
+        MyRigidbody.AddForce(new Vector2(0, 400));
+    }
+
+	public void spikerHurt(){
+		if (facingRight) {
+			MyRigidbody.AddForce(new Vector2(-300,100));
+			MyAnimator.SetTrigger("Hurt");
+			StartCoroutine(playerHurt());
+			GameObject.Find("Globals").GetComponent<globals>().health = GameObject.Find("Globals").GetComponent<globals>().health - 1;
+		}
+		if (!facingRight) {
+			MyRigidbody.AddForce(new Vector2(300,100));
+			MyAnimator.SetTrigger("Hurt");
+			StartCoroutine(playerHurt());
+			GameObject.Find("Globals").GetComponent<globals>().health = GameObject.Find("Globals").GetComponent<globals>().health - 1;
+		}
+	}
+
+	public IEnumerator playerHurt() {
+		hurtControlsOff = true;
+		GameObject.Find ("Globals").GetComponent<globals> ().playerHurtCooldown = true;
+		yield return new WaitForSeconds (0.50f);
+		GameObject.Find ("Player").GetComponent<SpriteRenderer> ().enabled = false;
+		yield return new WaitForSeconds (0.25f);
+		hurtControlsOff = false;
+		GameObject.Find ("Player").GetComponent<SpriteRenderer> ().enabled = true;
+		yield return new WaitForSeconds (0.25f);
+		GameObject.Find ("Player").GetComponent<SpriteRenderer> ().enabled =false;
+		yield return new WaitForSeconds (0.25f);
+		GameObject.Find ("Player").GetComponent<SpriteRenderer> ().enabled = true;
+		yield return new WaitForSeconds (0.25f);
+		GameObject.Find ("Player").GetComponent<SpriteRenderer> ().enabled = false;
+		yield return new WaitForSeconds (0.25f);
+		GameObject.Find ("Player").GetComponent<SpriteRenderer> ().enabled = true;
+		yield return new WaitForSeconds (0.25f);
+		GameObject.Find ("Player").GetComponent<SpriteRenderer> ().enabled = false;
+		yield return new WaitForSeconds (0.25f);
+		GameObject.Find ("Player").GetComponent<SpriteRenderer> ().enabled = true;
+		yield return new WaitForSeconds (0.25f);
+		GameObject.Find ("Globals").GetComponent<globals> ().playerHurtCooldown = false;
+	}
+	
+
+    public void KickReset() {
+        MyAnimator.SetBool("Kick", false);
+        kicking = false;
+    }
+}
---
+++ Assets/Scripts/globals.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class globals : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject textBox;
+
+	public bool playerHurtCooldown = false;
+
+	/*
+	[SerializeField]
+	private GameObject h1Obj;
+	[SerializeField]
+	private GameObject h2Obj;
+	[SerializeField]
+	private GameObject h3Obj;
+	[SerializeField]
+	private GameObject h4Obj;
+	*/
+
+    public int health = 5;
+	public float power = 50f;
+	public bool powerOn = false;
+	public float powerDrainSpeed = 2;
+	public float powerChargeSpeed = 2;
+
+    public int blueCollTotal = 0;
+    public int pinkCollTotal = 0;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		if (health == 5) {
+			h4Obj.SetActive(true);
+			h3Obj.SetActive(false);
+			h2Obj.SetActive(false);
+			h1Obj.SetActive(false);
+		}
+		if (health == 4) {
+			h4Obj.SetActive(false);
+			h3Obj.SetActive(true);
+			h2Obj.SetActive(false);
+			h1Obj.SetActive(false);
+		}
+		if (health == 3) {
+			h4Obj.SetActive(false);
+			h3Obj.SetActive(false);
+			h2Obj.SetActive(true);
+			h1Obj.SetActive(false);
+		}
+		if (health == 2) {
+			h4Obj.SetActive(false);
+			h3Obj.SetActive(false);
+			h2Obj.SetActive(false);
+			h1Obj.SetActive(true);
+		}
+		if (health == 1) {
+			h4Obj.SetActive(false);
+			h3Obj.SetActive(false);
+			h2Obj.SetActive(false);
+			h1Obj.SetActive(false);
+		}
+		*/
+
+		//GameObject.Find ("power").GetComponent<RectTransform> ().sizeDelta = new Vector2 (power, 9);
+
+		//if (!GameObject.Find ("controllerCollider").GetComponent<Player> ().hurtControlsOff) {
+		//	if (power <= 50f && !powerOn) {
+		//		power += Time.deltaTime * powerDrainSpeed;
+		//	}
+		//
+		//	if (Input.GetKey (KeyCode.P) && power > 0 && power < 51 && !playerHurtCooldown) {
+		//		power -= Time.deltaTime * powerChargeSpeed;
+		//		powerOn = true;
+		//	} else {
+		//		powerOn = false;
+		//	}
+		//
+		//	if (Input.GetKey (KeyCode.T) && power > 0 && power < 51) {
+		//		textBox.SetActive (true);
+		//	} else {
+		//		textBox.SetActive (false);
+		//	}
+		//}
+
+    }
+	
+}
